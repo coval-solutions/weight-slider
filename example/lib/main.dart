210f62a5aabce0b3c97f80e186ef5b851a5a813a
Diogo Costa <hello@diogothecoder.com>
2020-11-28 12:10:07 +0000

    BF-45: updated README and CHANGELOG

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -42,6 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
           minWeight: 40,
           maxWeight: 120,
           onChange: (val) => setState(() => this.weight = val),
+          unit: 'kg', // optional
         ),
       ),
     );
