3f1dbf05422b41a117b002369df97a969d8619d7
Diogo Costa <hello@diogothecoder.com>
2020-11-28 12:05:41 +0000

    BF-45: add example app

---
+++ example/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:weight_slider/weight_slider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(
+        title: 'Demo',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int weight = 70;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: WeightSlider(
+          weight: weight,
+          minWeight: 40,
+          maxWeight: 120,
+          onChange: (val) => setState(() => this.weight = val),
+        ),
+      ),
+    );
+  }
+}
